f3ca3813e00d5c54dd163ecaec5226a787cb7d54
Datskalf <70939999+Datskalf@users.noreply.github.com>
2022-04-04 21:22:35 +0200

    Added Spearhead gallery

--- games/snake/snake.js
+++ games/snake/snake.js
@@ -1,11 +1,4 @@
-// Creating the canvas in DOM
-gamearea = document.getElementById("game-area");
-canvas = document.createElement("canvas");
-gamearea.appendChild(canvas);
-
-canvas.style.width = "100%";
-canvas.style.height = "100%";
-
+canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 
@@ -14,6 +7,8 @@ var ctx = canvas.getContext("2d");
 // Game variables
 const boardWidth = 24;
 const boardHeight = 20;
+
+const frames = 2;
 
 const tileWidth = canvas.width / boardWidth;
 const tileHeight = canvas.height / boardHeight;
@@ -35,6 +30,7 @@ for (x = 0; x < boardWidth; x++) {
 					case "food":
 						return "#ff0000";
 					case "head":
+						return "#00dd00";
 					case "body":
 						return "#00ff00";
 					default:
@@ -43,6 +39,12 @@ for (x = 0; x < boardWidth; x++) {
 			}
 		};
 	}
+}
+
+var snake = {
+	headLocation: [4, 6],
+	bodyLength: 2,
+	bodyPositions: [[3, 6], [2, 6]]
 }
 
 // Base game functions
@@ -66,11 +68,7 @@ function resetBoard() {
 function drawBoard() {
 	resetBoard();
 
-	board[2][2].type = "head";
-	board[2][3].type = "body";
-	board[2][4].type = "body";
-	board[2][5].type = "body";
-	board[2][6].type = "body";
+
 
 	for (x = 0; x < boardWidth; x++){
 		for (y = 0; y < boardHeight; y++){
@@ -80,4 +78,42 @@ function drawBoard() {
 	}
 }
 
+
+// Creates the starting body
+board[2][2].type = "head";
+board[2][3].type = "body";
+board[2][4].type = "body";
+board[2][5].type = "body";
+board[3][5].type = "body";
+board[4][5].type = "body";
+
 drawBoard();
+
+window.onkeypress = function(event) {
+	console.log(event.keyCode);
+	let headloc = getHeadLocation();
+	switch (event.keyCode) {
+		case 87: // W
+		case 119: // w
+			board[headloc[0]][headloc[1]].heading = "north";
+			break;
+		case 65: // A
+		case 97: // a
+			board[headloc[0]][headloc[1]].heading = "west";
+			break;
+		case 83: // S
+		case 115: // s
+			board[headloc[0]][headloc[1]].heading = "south";
+			break;
+		case 68: // D
+		case 100: // d
+			board[headloc[0]][headloc[1]].heading = "east";
+			break;
+	}
+}
+
+// Movement interval
+window.setInterval(function(){
+	let headloc = getHeadLocation()
+	
+}, 1000/frames);
