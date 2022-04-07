fcb34a577eaf3933ebf7b25dfb0b97f7e9eb8841
Datskalf <70939999+Datskalf@users.noreply.github.com>
2022-04-07 11:33:04 +0200

    Delete filename.py

--- Spearhead/Images/filename.py
+++
@@ -1,6 +0,0 @@
-import os, pathlib
-
-files = os.listdir(os.path.join(pathlib.Path(__file__).parent, 'Tine Joramo'))
-
-for i, file in enumerate(files):
-	os.rename(os.path.join(pathlib.Path(__file__).parent, 'Tine Joramo', file), os.path.join(pathlib.Path(__file__).parent, 'Tine Joramo', 'TJ_{}.jpg'.format(i+1)))
