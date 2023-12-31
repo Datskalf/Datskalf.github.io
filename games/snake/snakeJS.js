21d7a43ad32ea0e4d1f32a2446e82ec0345918b2
Sondre <datskalf@outlook.com>
2023-12-31 15:14:26 +0000

    Dunno

---
+++ games/snake/snakeJS.js
@@ -0,0 +1,18 @@
+let gameArea = document.getElementById("gameArea")
+
+function updateGameAreaSize() {
+    let totalWidth = window.innerWidth;
+    let totalHeight = window.innerHeight;
+
+    if (totaiHeight == 0) {
+        return;
+    }
+
+    if (totalWidth / totalHeight >= 2) {}
+}
+
+
+
+
+
+addEventListener("resize", updateGameAreaSize)
